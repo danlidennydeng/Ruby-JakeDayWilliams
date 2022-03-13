9b83c49699a6263e4b45219eb82d6e0f575e7dec
Danli Denny Deng <danlideng@yahoo.com>
2022-03-13 01:44:54 +0000

    clean up

---
+++ sort_list.rb
@@ -0,0 +1,15 @@
+# def sortedlist(array, reverse = false)
+
+#   if reverse == false
+#     array.sort {|a,b| a <=> b} 
+
+#   else
+#     array.sort {|a,b| b <=> a}
+#   end
+# end
+
+# words = ["linda", "Bob", "Denny", "Henry"]
+
+# puts "proper order: #{sortedlist(words, true)}"
+
+puts 400 < 200 ? "True" : "False"
